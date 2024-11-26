e44672654ed91891b2038b1d8cc0141e754ac76b
ahmadezzat472 <admedo472@gmail.com>
2024-11-26 19:37:05 +0200

    Display Cart Items In Cart Drawer

--- src/components/CartDrawer.tsx
+++ src/components/CartDrawer.tsx
@@ -7,14 +7,18 @@ import {
     DrawerCloseButton,
     Button,
     DrawerBody,
+    Text,
 } from '@chakra-ui/react'
 import { useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { isOpenCartDrawerAction, selectGlobal } from '../app/features/globalSlice'
+import CartDrawerItem from './CartDrawerItem'
+import { ICartProducts, selectCart } from '../app/features/cartSlice'
 
 const CartDrawer = () => {
     const btnRef = useRef<HTMLButtonElement | null>(null)
     const {isOpenCartDrawer} = useSelector(selectGlobal)
+    const {cartItems} = useSelector(selectCart)
     const dispatch = useDispatch()
 
     const onClose = () => dispatch(isOpenCartDrawerAction())
@@ -31,6 +35,13 @@ const CartDrawer = () => {
                 <DrawerHeader borderBottomWidth='1px'>Cart Items</DrawerHeader>
 
                 <DrawerBody>
+                    {
+                        cartItems.length ? (
+                            cartItems.map( (item: ICartProducts) => <CartDrawerItem key={item.id} item={item} />)
+                        ) : (
+                            <Text>No Cart tems</Text>
+                        )
+                    }
                 </DrawerBody>
 
                 <DrawerFooter>
---
+++ src/components/CartDrawerItem.tsx
@@ -0,0 +1,42 @@
+import { Badge, Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
+import { ICartProducts } from '../app/features/cartSlice'
+
+interface IProps {
+    item: ICartProducts
+}
+
+const CartDrawerItem = (item: IProps) => {
+    // const  = item
+    const {title, price, quantity, thumbnail} = item.item
+    /* ___________________ State ___________________ */
+    const serverUrl = import.meta.env.VITE_SERVER_URL
+
+    return (
+        <Card 
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='outline' 
+            mt={"10px"}
+        >
+            <Image w={"40%"} h={"80px"} src={`${serverUrl}${thumbnail.url}`} />
+            <HStack flexGrow={"2"} >
+                <CardBody padding={"8px"} flexGrow={"2"}>
+                    <Heading size='xs'>{title}</Heading>
+                    <HStack mt={"8px"} justifyContent={"space-between"} alignItems={"center"} flexWrap={"wrap"}>
+                        <Text fontWeight={"bold"} fontSize={"xs"}>
+                            Quantity: {quantity} 
+                        </Text>
+                        <Badge fontSize={"xs"}>${price}</Badge>
+                    </HStack>
+                    {/* <Text 
+                        fontWeight={"bold"} fontSize={"xs"}
+                    >
+                        data.price
+                    </Text> */}
+                </CardBody>
+            </HStack>
+        </Card>
+    )
+}
+
+export default CartDrawerItem
